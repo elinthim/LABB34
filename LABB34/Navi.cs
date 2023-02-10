c23f668b19ac2f993134a89de179440c32348437
elinthim <elin_thim@hotmail.com>
2023-02-10 12:52:19 +0100

    stilltryin

--- LABB34/Navi.cs
+++ LABB34/Navi.cs
@@ -3,9 +3,8 @@ using LABB34.Models;
 
 namespace LABB34
 {
-    internal class Navi
+    public class Navi
     {
-        //public class Start        
         public void Start()
         {
             while (true)
@@ -25,11 +24,11 @@ namespace LABB34
                 int option = int.Parse(Console.ReadLine());
                 Console.Clear();
 
-                //switch (option)
-                int input = checkNr();
-                //switch (input)
-                var choice = Console.ReadLine();
-                switch (choice)
+                switch (option)
+                //int input = checkNr();
+                //switch (input); 
+                //var choice = Console.ReadLine();
+                //switch (choice)
                 {
                     case 1:
                         ChooseAClass();
@@ -44,16 +43,19 @@ namespace LABB34
                         break;
 
                     case 4:
-                        //Subject();
+                        StudentInfo(); 
                         break;
+
                     case 5:
-                        StaffId();
+                        Subject(); 
                         break;
+
                     case 6:
-                        //StudentInfo();
+                        Grades();
                         break;
+
                     case 7:
-                        //Grades();
+                        StaffId(); 
                         break;
 
                     case 8:
@@ -63,123 +65,245 @@ namespace LABB34
                 Console.WriteLine("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>");
             }
         }
+    }
 
+    public void ChooseAClass()
+    {
 
-
-        public void SeeAllStudents()
+        using (var sc = new Labb4Context())
         {
-            Console.WriteLine("< -------------------------------------------- >");
-            Console.WriteLine("|  Choose to see students by first or lastname |");
-            Console.WriteLine("|  [1]   See students sorted by firstname      |");
-            Console.WriteLine("|  [2]   See students sorted by lastname       |");
-            Console.WriteLine("< -------------------------------------------- >");
-            Console.Write(" [1] firstname, [2] lastname : ");
-            int input = int.Parse(Console.ReadLine());
-            //int input = checkNr();
-            Console.Clear();
-
-            if (input == 1)
+            var students = from c in sc.Students
+                           select c;
+            foreach (var it in students)
             {
-                Console.WriteLine("Choose sorting order for the students in firstname and in order by rising or falling");
-                Console.WriteLine("[1] Rising");
-                Console.WriteLine("[2] Falling");
-                //int choice = checkNr();
-                //int choice = checkNr();
-                int anwser = int.Parse(Console.ReadLine());
-                if (answer == 1)
-                {
-                    using (var db = new Labb4Context())
-                    {
-                        var SeeAllStudents = from a in db.Students orderby a.FirstName select a;
-                        Console.WriteLine("");
-                        foreach (var item in SeeAllStudents)
-                        {
-                            Console.WriteLine(item.FirstName + " " + item.LastName);
-
-                        }
-                    }
-                    Console.WriteLine("Enter will return you to the meny");
-                    Console.ReadKey();
-                    Console.Clear();
-                }
-                else if (choice == 2)
-                {
-                    using (var db = new Labb4Context())
-                    {
-                        var SeeAllStudents = from a in db.Students.OrderByDescending(a => db.StudentContactLists) select a;
-                        Console.WriteLine("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-                        foreach (var it in SeeAllStudents)
-                        {
-
-                            Console.WriteLine(it.FirstName + " " + it.LastName);
-                        }
-                    }
-                    Console.WriteLine("Enter will return you to the meny");
-                    Console.ReadKey();
-                    Console.Clear();
-                }
+                Console.WriteLine($"Classname:{it.Class} StudentId:{it.StudentId}");
             }
-            else if (input == 2)
+            Console.WriteLine("Write class you want to look at");
+            int option = int.Parse(Console.ReadLine());
+            Console.WriteLine("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+
+            var Students1 = from s in sc.Students
+                            join c in sc.Students on s.StudentId equals c.Class
+                            where c.StudentId == option
+                            select new
+                            {
+                                c.StudentId,
+                                c.Class1,
+                                s.FirstName,
+                                s.LastNname
+                            };
+
+            foreach (var it in Students1)
             {
-                Console.WriteLine("Choose sorting by lastname rising or falling");
-                Console.WriteLine("[1] Sorting lastname by rising");
-                Console.WriteLine("[2] Sorting firstname by rising");
-                int anwser = int.Parse(Console.ReadLine());
-                if (anwser == 1)
-                {
-                    using (var db = new Labb4Context())
-                    {
-                        var Student = from a in StudentContactList orderby a.LastName select a;
-                        Console.WriteLine("");
-                        foreach (var item in StudentContactList)
-                        {
-                            Console.WriteLine(item.LastName + " " + item.FirstName);
-
-                        }
-                    }
-                    Console.WriteLine("Enter will return you to the meny");
-                    Console.ReadKey();
-                    Console.Clear();
-                }
-                else if (anwser == 2)
-                {
-                    using (var db = new Labb4Context())
-                    {
-                        var Student = from a in StudentContactList.OrderByDescending(a => a.FirstName) select a;
-                        Console.WriteLine("");
-                        foreach (var item in Student)
-                        {
-
-                            Console.WriteLine(item.LastName + " " + item.FirstName);
-                        }
-                    }
-                    Console.WriteLine("Enter will return you to the meny");
-                    Console.ReadKey();
-                    Console.Clear();
-                }
+                Console.WriteLine($"classid:{it.StudentId} class:{it.Class1} FirstName:{it.FirstName} LastName:{it.LastName}");
             }
-
-        }
-        public void AddStaffMember()
-        {
-            using Labb4Context = new Labb4Context();
-            StaffContactList e1 = new StaffContactList();
-            Console.Write("Firstname : ");
-            e1.FirstName = Console.ReadLine();
-            Console.Write("Lastname : ");
-            e1.LastName = Console.ReadLine();
-            Console.Write("Work title : ");
-            e1.FkStaff = Console.ReadLine();
-            //Labb4Context-Staff.Add(e1);
-            //Labb4Context.SaveChanges();
-            Console.WriteLine("Database updated");
-            Console.WriteLine("TrEnter will return you to the meny");
+            Console.WriteLine("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+            Console.WriteLine("Enter returns you to meny");
             Console.ReadKey();
             Console.Clear();
+
         }
 
+    }
+
+    public void SeeAllStudents()
+    {
+        Console.WriteLine("< -------------------------------------------- >");
+        Console.WriteLine("|  Choose to see students by first or lastname |");
+        Console.WriteLine("|  [1]   See students sorted by firstname      |");
+        Console.WriteLine("|  [2]   See students sorted by lastname       |");
+        Console.WriteLine("< -------------------------------------------- >");
+        Console.Write(" [1] firstname, [2] lastname : ");
+        int input = int.Parse(Console.ReadLine());
+        //int input = checkNr();
+        Console.Clear();
+
+        if (input == 1)
+        {
+            Console.WriteLine("Choose sorting order for the students in firstname and in order by rising or falling");
+            Console.WriteLine("[1] Rising");
+            Console.WriteLine("[2] Falling");
+            int choice = checkNr();
+            int anwser = int.Parse(Console.ReadLine());
+            if (answer == 1)
+            {
+                using (var db = new Labb4Context())
+                {
+                    var SeeAllStudents = from a in db.Students orderby a.FirstName select a;
+                    Console.WriteLine("");
+                    foreach (var item in SeeAllStudents)
+                    {
+                        Console.WriteLine(item.FirstName + " " + item.LastName);
+
+                    }
+                }
+                Console.WriteLine("Enter will return you to the meny");
+                Console.ReadKey();
+                Console.Clear();
+            }
+            else if (choice == 2)
+            {
+                using (var db = new Labb4Context())
+                {
+                    var SeeAllStudents = from a in db.Students.OrderByDescending(a => db.StudentContactLists) select a;
+                    Console.WriteLine("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+                    foreach (var it in SeeAllStudents)
+                    {
+
+                        Console.WriteLine(it.FirstName + " " + it.LastName);
+                    }
+                }
+                Console.WriteLine("Enter will return you to the meny");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+        else if (input == 2)
+        {
+            Console.WriteLine("Choose sorting by lastname rising or falling");
+            Console.WriteLine("[1] Sorting lastname by rising");
+            Console.WriteLine("[2] Sorting firstname by rising");
+            int anwser = int.Parse(Console.ReadLine());
+            if (anwser == 1)
+            {
+                using (var db = new Labb4Context())
+                {
+                    var SeeAllStudents = from a in Students orderby a.LastName select a;
+                    Console.WriteLine("");
+                    foreach (var item in SeeAllStudents)
+                    {
+                        Console.WriteLine(item.LastName + " " + item.FirstName);
+
+                    }
+                }
+                Console.WriteLine("Enter will return you to the meny");
+                Console.ReadKey();
+                Console.Clear();
+            }
+            else if (anwser == 2)
+            {
+                using (var db = new Labb4Context())
+                {
+                    var Student = from a in StudentContactList.OrderByDescending(a => a.FirstName) select a;
+                    Console.WriteLine("");
+                    foreach (var item in Student)
+                    {
+
+                        Console.WriteLine(item.LastName + " " + item.FirstName);
+                    }
+                }
+                Console.WriteLine("Enter will return you to the meny");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
 
     }
 
 
+    public void AddStaffMember()
+    {
+        using Labb4Context context = new Labb4Context();
+        StaffContactList e1 = new StaffContactList();
+        Console.Write("Firstname : ");
+        e1.FirstName = Console.ReadLine();
+        Console.Write("Lastname : ");
+        e1.LastName = Console.ReadLine();
+        Console.Write("Work title : ");
+        //e1.Staff = Console.ReadLine();
+        context.StaffContactLists.Add(e1);
+        context.SaveChanges();
+        Console.WriteLine("Database updated");
+        Console.WriteLine("Enter will return you to the meny");
+        Console.ReadKey();
+        Console.Clear();
+    }
+
+    public void StudentInfo()
+    {
+        using (var sc = new Labb4Context())
+        {
+            var Students1 = from s in sc.Students
+                        join c in sc.Class on s.FkStudentId equals c.ClassId
+                        select new
+                        {
+                            c.StudentId,
+                            c.Class1,
+                            s.Firstname,
+                            s.Lastname
+                        };
+
+            Console.WriteLine("");
+            foreach (var it in Students1)
+            {
+                Console.WriteLine($"classid:{it.ClassId} classname:{it.Class1} Firstname:{it.Firstname} Lastname:{it.Lastname}");
+            }
+            Console.WriteLine("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+            Console.WriteLine("Tryck Enter för att återvända till menyn");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+    }
+
+    public void Grades()
+    {
+        using (var sc = new Labb4Context())
+        {
+            var Students1 = from e in sc.Grades
+                        select new
+                        {
+                            e.Grades,
+                        };
+
+            Console.WriteLine("Active grades");
+            Console.WriteLine("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+            foreach (var it in Students1)
+            {
+                Console.WriteLine($"{it.Grades}");
+            }
+            Console.WriteLine("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+            Console.WriteLine("Enter returns you to the meny");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+    }
+    public void StaffId()
+    {
+        using (var sc = new Labb4Context())
+        {
+            var staff = from c in sc.staff
+                        select c;
+            foreach (var it in staff)
+            {
+                Console.WriteLine($"{it.StaffContactLists}");
+            }
+            Console.WriteLine("Write what teacher you want to se information about");
+            string option = (Console.ReadLine());
+            Console.WriteLine("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+
+            var staff1 = from s in sc.StaffContactLists
+                         where s. == option
+                         select new
+                         {
+                             s.FkStaffId,
+                             s.SocialSecufrityNumber,
+                             s.FirstName,
+                             s.LastName,
+                             s.Startdate,                            
+                         };
+
+            foreach (var it in staff1)
+            {
+                Console.WriteLine($"FkstaffId:{it.FkStaffId} Socialsecuritynumber:{it.SocialSecufrityNumber} Firstname:{it.FirstName} Lastname:{it.LastName} Startdate: {it.Startdate}");
+            }
+            Console.WriteLine("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+            Console.WriteLine("Enter returns you to meny");
+            Console.ReadKey();
+            Console.Clear();
+
+        }
+    }
 }
+
--- LABB34/Program.cs
+++ LABB34/Program.cs
@@ -22,3 +22,85 @@ namespace LABB34
         }
     }
 }
+
+// Lab 4
+// nr 1
+
+// See all staffmembers
+/*
+Select concat(Fname,' ', Lname) As Name, WorkTitle, DATEDIFF(year, HiredDate, '2022-12-20') As Years  From Employee
+Join Title on TitleId = FK_TitleId
+join SubjectSchool on SubjectSchoolId = FK_SchoolSubjectId
+--Where WorkTitle = 'Nurse'-- Principal, Nurse, Janitor, Additional_Staff
+order by Years DESC;
+*/
+
+// newstaffmenmber
+
+/*
+Insert into Employee (FName,LName,DateOfBirth,HiredDate,PhoneNr,Sex)
+Values('Noomi','Rapace','1968-04-04','2023-01-02','070-2848787','F') 
+*/
+
+
+// nr 2
+// See students grades staff member who put grades and date for grades
+/*
+ Select concat (Student.Fname,' ', Student.LName) As Student,Class, DateOfGrade, GivenGrade, Lesson ,concat (Employee.FName, ' ' ,Employee.LName) As Teacher From Grade
+Join Student on StudentId = FK_StudentId
+join Employee on EmployeeId = FK_EmployeeId
+join SubjectSchool on SubjectSchoolId = FK_SchoolSubjectId
+join Class on ClassId = FK_ClassId
+order by Class  ASC 
+*/
+
+// See students grades
+/*
+ Select concat (Student.Fname,' ', Student.LName) As Student,Class, DateOfGrade, GivenGrade, Lesson ,concat (Employee.FName, ' ' ,Employee.LName) As Teacher From Grade
+Join Student on StudentId = FK_StudentId
+join Employee on EmployeeId = FK_EmployeeId
+join SubjectSchool on SubjectSchoolId = FK_SchoolSubjectId
+join Class on ClassId = FK_ClassId
+where Student.FName = 'Hedvig'
+*/
+
+// Vilken klass eleven går i
+/*
+ select CONCAT (Student.Fname, ' ' , Student.LName) As Name ,Class From  Student
+Join Class on ClassId = FK_ClassId
+where FName = 'Lena'
+*/
+
+
+// nr 6 och 7
+// salary
+/*
+ Select  SUM(Wage) AS Total_Wage, WorkTitle From Employee
+join Title on TitleId = FK_TitleId
+group by WorkTitle
+order by Total_Wage DESC
+*/
+
+// Medellön på alla avdelningar
+/*
+ Select  Avg(Wage) AS Average_Wage,WorkTitle From Employee
+join Title on TitleId = FK_TitleId
+group by WorkTitle
+order by Average_Wage DESC 
+*/
+
+
+// nr 9 Transaction
+/*
+ Begin Transaction Grade1 
+Begin Try
+update Grade
+set GivenGrade = 5, DateOfGrade = GETDATE()
+where GradeId = 1
+Commit Transaction Grade1
+End Try
+Begin Catch
+Rollback Transaction Grade1
+End Catch
+--select * from Grade
+*/
